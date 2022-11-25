f95999c671a256b1ecd979027306df623ae25064
brunotecnqn <91470629+brunotecnqn@users.noreply.github.com>
2022-11-25 15:57:42 -0300

    Update listarUsuario.php

--- Vista/usuario/listarUsuario.php
+++ Vista/usuario/listarUsuario.php
@@ -5,7 +5,7 @@ include_once $dir."../Vista/estructura/headerSeguro.php";
 include_once '../../configuracion.php';
 
 ?>
-<link rel="stylesheet" href="../vista/css/bootstrap/4.5.2/bootstrap.min.css">
+
 <div class="container border border-secondary principal mt-3 pt-3">
    <h3 class="text-center">Listado de Usuarios</h3>
     <div class="row text-muted m-0">
@@ -18,7 +18,7 @@ include_once '../../configuracion.php';
         //print_r($listaUsuario);
         if(count($listaUsuario)>0){
             ?>
-            <table class="table table-dark table-striped text-center table-hover" cellspacing="0" width="100%">
+            <table class="table table-light table-striped text-center table-hover" cellspacing="0" width="100%">
                 <thead>
                     <tr>
                         <th scope="col">ID Usuario</th>
@@ -26,9 +26,9 @@ include_once '../../configuracion.php';
                         <!--<th scope="col">Contraseña</th>-->
                         <th scope="col">Correo Electrónico</th>
                         <th scope="col">Hab/Deshabilitado</th>
-                        <!--
+                      
                         <th scope="col"></th>
-                        <th scope="col"></th>
+                          <!--<th scope="col"></th>
                         -->                
                     </tr>
                 </thead>
@@ -46,8 +46,10 @@ include_once '../../configuracion.php';
                         <td>'.$objUsuario->getusmail().'</td>';
                         echo '
                         <td>'.$objUsuario->getusdeshabilitado().'</td>';
-
-                       // echo '<td><a href="editarUsuario.php?accion=editar&idusuario='.$objUsuario->getidusuario().'" class="btn btn-success">Editar</a></td>';
+                         if($objUsuario->getusdeshabilitado()==null)
+                        echo '<td><a href="editarUsuario.php?accion=editar&idusuario='.$objUsuario->getidusuario().'" class="btn btn-success">Dar rol</a></td>';
+                        else
+                        echo '<td></td>';
                         //echo '<td><a href="accion/eliminar_usuario.php?idusuario'.$objUsuario->getidusuario().'" class="btn btn-success">Deshabilitar</a></td>';
                        /*  echo '<td><a href="accionBuscarPersona.php?NroDni='.$objTabla->getNroDni().'" class="btn btn-primary"><i class="bi bi-pencil-square"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
