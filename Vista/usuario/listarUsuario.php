0180bb96f5be10a1f2548675cc7ff27de1da7050
brunotecnqn <91470629+brunotecnqn@users.noreply.github.com>
2022-12-04 07:17:48 -0300

    Algunos cambios para listar usuario

--- Vista/usuario/listarUsuario.php
+++ Vista/usuario/listarUsuario.php
@@ -8,77 +8,24 @@ include_once '../../configuracion.php';
 
 <div class="container border border-secondary principal mt-3 pt-3">
    <h3 class="text-center">Listado de Usuarios</h3>
-    <div class="row text-muted m-0">
-        <?php 
-        
-        
-        $objAbmUsuario = new ABMUsuario();
-
-        $listaUsuario = $objAbmUsuario->buscar(null);
-        //print_r($listaUsuario);
-        if(count($listaUsuario)>0){
-            ?>
-            <table class="table table-light table-striped text-center table-hover" cellspacing="0" width="100%">
-                <thead>
-                    <tr>
-                        <th scope="col">ID Usuario</th>
-                        <th scope="col">Nombre y Apellido</th>
-                        <!--<th scope="col">Contraseña</th>-->
-                        <th scope="col">Correo Electrónico</th>
-                        <th scope="col">Hab/Deshabilitado</th>
-                      
-                        <th scope="col"></th>
-                          <!--<th scope="col"></th>
-                        -->                
-                    </tr>
-                </thead>
-                <tbody>
-            <?php
-                    
-                    foreach ($listaUsuario as $objUsuario) {                         
-                        echo '<tr>
-                            <th scope="row">'.$objUsuario->getidusuario().'</th>';
-                        echo '
-                             <td>'.$objUsuario->getusnombre().'</td>';
-                       /* echo '
-                        <td>'.$objUsuario->getuspass().'</td>';*/
-                        echo '
-                             <td>'.$objUsuario->getusmail().'</td>';
-                    
-                        echo '
-                             <td id="listarUsuario">'.$objUsuario->getusdeshabilitado().'</td>';
-                         if($objUsuario->getusdeshabilitado()==null){
-                            echo '<td><a href="asignarRol.php?idusuario='.$objUsuario->getidusuario().'" class="btn btn-success">Dar rol</a> ';  
-                            ?>
-                            <a href="javascript:void(0)" class="btn btn-dark" role="button" onclick="desHabUsuario(<?php echo $objUsuario->getidusuario(); ?>)">Deshabilitar</a></td>;
-                            <?php
-                         }else{
-                            echo '<td></td>';
-                       // echo '<td><a href="accion/eliminar_usuario.php?idusuario'.$objUsuario->getidusuario().'" class="btn btn-dark">Deshabilitar</a></td>';                       
-                            echo'</tr>';
-                         }
-                     }                     
-                //fin foreach
-                echo '    </tbody>
-            </table>';
-        }
-        else{
-            echo "<h3>No hay personas registradas </h3>";
-        }                
-        ?>    
+    <div id="listarUsuario" class="row text-muted m-0">
+   
      </div>    
  </div>
  <!--<div id="listarUsuario"class="form-group mb-4">
                 
  </div>--> 
-        <script type="text/javascript">
+       
+        </div>
+    <div>
+    <script type="text/javascript">
              $(document).ready(function(){
             cargarUsuarios();
           });
 
           function cargarUsuarios()
           {
-            $("#listarUsuario").load('accion/listar_usuario.php?idusuario='+$("#idusuario").val());
+            $("#listarUsuario").load('accion/listar_usuario.php');
          
           }
 
@@ -121,8 +68,6 @@ include_once '../../configuracion.php';
             }
 
             </script>
-        </div>
-    <div>
 <?php
 include ("../../Vista/estructura/footer.php");
 ?>
